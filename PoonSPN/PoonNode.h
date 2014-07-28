993f7ee7b8ce9a5832fd67695d658e4178fc2a87
Arvie Frydenlund <aaf955@mail.usask.ca>
2014-07-28 17:41:18 -0400

    node class

--- PoonSPN/PoonNode.h
+++ PoonSPN/PoonNode.h
@@ -1,8 +1,37 @@
 #pragma once
+
+#include <limits>  
+#include <math.h> 
+//an abstract node class
 class PoonNode
 {
+
+	double ZERO_LOGVAL_;
+
+	double logval_;
+	double logDerivative_;
+
 public:
-	PoonNode();
+	PoonNode(){
+		ZERO_LOGVAL_ = -std::numeric_limits<double>::infinity(); //negative infinity
+		logDerivative_ = ZERO_LOGVAL_;
+	};
 	~PoonNode();
+
+	double getLogVal() { return logval_; };
+	void setVal(double v) { 
+		if (v == 0){
+			logval_ = ZERO_LOGVAL_;
+		}
+		else{
+			logval_ = log(v);
+		}
+	};
+
+	// evaluate root
+	virtual void eval() = 0;
+
+	// propagate derivative to children
+	virtual void passDerivative() = 0;
 };
 

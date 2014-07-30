29d5391192f875c257711596ea2aeb86db02545d
Arvie Frydenlund <aaf955@mail.usask.ca>
2014-07-29 20:10:32 -0400

    sum node

--- PoonSPN/PoonSumNode.cpp
+++ PoonSPN/PoonSumNode.cpp
@@ -1,12 +1,74 @@
-#include "stdafx.h"
+
 #include "PoonSumNode.h"
+#include <cmath>
+#include "Utils.h"
 
 
-PoonSumNode::PoonSumNode()
-{
+void PoonSumNode::eval() {
+	double v = 0;
+	string maxi;
+	double maxl = 0;
+	for (auto& kv : chds_) { //c++11 key value pairs
+		string i = kv.first;
+		double l = kv.second.getLogVal();
+		if (l != this->ZERO_LOGVAL_){
+			if (maxi.empty() || maxl < l) {
+				maxi = i; 
+				maxl = l;
+			}
+		}
+	}
+	if (maxi.empty()){
+		this->setVal(this->ZERO_LOGVAL_);
+		return; 
+	}
+	for (auto& kv : chds_) {
+		string i = kv.first;
+		if (chdCnts_.find(i) == chdCnts_.end())
+			continue;
+		double l = l = kv.second.getLogVal();
+		if (l == this->ZERO_LOGVAL_) 
+			continue;
+		v += getChdCnt(i)*exp(l - maxl);
+	}
+	this->setVal(log(v / cnt_) + maxl);
 }
 
+void PoonSumNode::passDerivative() {
+	if (logDerivative_ == this->ZERO_LOGVAL_) 
+		return;
 
-PoonSumNode::~PoonSumNode()
-{
+	for (auto& kv : chds_) {
+		string di = kv.first;
+		PoonNode& n = kv.second;
+		double l = logDerivative_ + log(getChdCnt(di) / cnt_);
+		if (n.getLogDerVal() == this->ZERO_LOGVAL_)
+			n.setLogDerVal(l);
+		else
+			n.setLogDerVal(addLog(l, n.getLogVal()));
+	}
+}
+
+void PoonSumNode::addChdOnly(string decompIdx, double cnt, PoonNode& n) {
+	if (chds_.find(decompIdx) == chds_.end()) {
+		chds_[decompIdx] = n;
+	}
+	if (chdCnts_.find(decompIdx) == chdCnts_.end()) {
+		chdCnts_[decompIdx] = cnt;
+	}
+	else 
+		chdCnts_[decompIdx] = cnt + getChdCnt(decompIdx);
+	cnt_ += cnt;
+}
+
+void PoonSumNode::removeChdOnly(string decompIdx, double cnt) {
+	double cc = getChdCnt(decompIdx);
+	cc -= cnt;
+	if (cc == 0) { //double test if zero, bad
+		chds_.erase(decompIdx);
+		chdCnts_.erase(decompIdx);
+	}
+	else 
+		chdCnts_[decompIdx] = cc;
+	cnt_ -= cnt;
 }
--- PoonSPN/PoonSumNode.h
+++ PoonSPN/PoonSumNode.h
@@ -1,10 +1,36 @@
 #pragma once
-#include "PoonNode"
+#include "PoonNode.h"
+#include "PoonParameter.h"
+#include <map>
+#include <string>
 
 class PoonSumNode : public PoonNode
 {
 public:
-	PoonSumNode();
-	~PoonSumNode();
+	std::map<string, PoonNode> chds_;
+	std::map<string, double> chdCnts_;
+	double cnt_;
+
+	PoonSumNode(PoonParameter& param) {
+		cnt_ = param.smoothSumCnt_;
+	}
+
+	void eval();
+
+	void passDerivative();
+
+	double getChdCnt(string di) {
+		return chdCnts_.at(di);
+	}
+
+	void setChdCnt(string di, double cnt){
+		chdCnts_[di] = cnt;
+	}
+
+	void removeChdOnly(string decompIdx, double cnt);
+
+	void PoonSumNode::addChdOnly(string decompIdx, double cnt, PoonNode& n);
+
+	void PoonSumNode::removeChdOnly(string decompIdx, double cnt);
 };
 
